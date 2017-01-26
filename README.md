d1636f7303bda071d0c0c705b1c70f5fd0dbd498
Richard Outten (mbp) <outtenr@gmail.com>
2017-01-26 12:29:14 -0500

    Initial import
    This is a rough working version. Need to update the README more.

---
+++ README.md
@@ -0,0 +1,15 @@
+# glab
+
+A command line tool for querying information out of a gitlab instance.
+
+# Setup
+
+Add the following environmental variables:
+
+    GLAB_HOST
+    GLAB_APIPATH
+    GLAB_TOKEN
+    GLAB_DELAY
+
+
+
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	simplejson "github.com/bitly/go-simplejson"
+	"github.com/justincampbell/anybar"
+	"github.com/namsral/flag"
+)
+
+type argsContext struct {
+	Args    []string
+	Host    *string
+	Token   *string
+	APIPath *string
+	Delay   *string
+}
+
+func (ac *argsContext) todoURL() string {
+	return fmt.Sprintf("%s%stodos", *ac.Host, *ac.APIPath)
+}
+
+func (ac *argsContext) valid() bool {
+	valid := true
+	if *ac.Host == "" || *ac.Token == "" || *ac.APIPath == "" {
+		valid = false
+	}
+	return valid
+}
+
+func parseArgs(args []string) *argsContext {
+	fs := flag.NewFlagSetWithEnvPrefix(args[0], "GLAB", flag.ExitOnError)
+
+	ap := &argsContext{
+		Args:    args,
+		Host:    fs.String("host", "", "name of the gitlab host"),
+		APIPath: fs.String("apipath", "", "api path on the gitlab host"),
+		Token:   fs.String("token", "", "token for gitlab"),
+		Delay:   fs.String("delay", "60s", "Delay between polling gitlab. default: 60s"),
+	}
+	fs.Parse(args)
+	if !ap.valid() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fs.PrintDefaults()
+		os.Exit(1)
+	}
+
+	return ap
+}
+
+func checkTodos(ac *argsContext) {
+
+	url := ac.todoURL()
+	fmt.Printf("URL: %s\n", url)
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("PRIVATE-TOKEN", *ac.Token)
+
+	response, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+	buf, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	j, err := simplejson.NewJson(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	vals, err := j.Array()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", len(vals))
+	if len(vals) > 0 {
+		anybar.Red()
+	} else {
+		anybar.White()
+	}
+}
+
+func main() {
+	ac := parseArgs(os.Args)
+
+	fmt.Printf("%+v\n", ac)
+
+	for {
+		checkTodos(ac)
+		t, _ := time.ParseDuration(*ac.Delay)
+		time.Sleep(t)
+	}
+
+}
